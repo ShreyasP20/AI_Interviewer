9004a35bc77068f9d4dedf8bedd4413c126320a0
Shreyas Patil <luciferjones2015@gmail.com>
2024-04-27 15:09:33 +0530

    Basic Shit Done!

--- AiApp/IntervueApp/urls.py
+++ AiApp/IntervueApp/urls.py
@@ -4,5 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('', views.index, name='index'),
+    path('record_and_process_audio/', views.record_and_process_audio, name='record_and_process_audio'),
+    path('play_audio', views.play_audio, name='play_audio'),
     path('upload', views.upload, name='upload')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- AiApp/IntervueApp/views.py
+++ AiApp/IntervueApp/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from .models import Multiple
 import os
 from django.conf import settings
+from django.http import JsonResponse
 # Create your views here.
 
 def index(request):
@@ -19,3 +20,24 @@ def upload(request):
         #             destination.write(chunk)
                     
     return render(request, 'IntervueApp/upload.html')
+
+def record_and_process_audio(request):
+    if request.method == 'GET':
+        return render(request, 'IntervueApp/record_audio.html')
+    elif request.method == 'POST':
+        if request.FILES.get('audio'):
+            audio_file = request.FILES['audio']
+            os.makedirs(os.path.dirname(settings.MEDIA_ROOT + '\\audio\\audio.wav'), exist_ok=True)
+            with open(settings.MEDIA_ROOT + '\\audio\\audio.wav', 'wb') as f:
+                for chunk in audio_file.chunks():
+                    f.write(chunk)
+            return redirect(play_audio)
+        else:
+            return JsonResponse({'status': 'error', 'message': 'No audio file found'})
+    else:
+        return JsonResponse({'status': 'error', 'message': 'Unsupported request method'})
+    
+    
+def play_audio(request):
+    if request.method == "GET":
+        return render(request, 'IntervueApp/play_audio.html')
