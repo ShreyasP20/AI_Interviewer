078693f221c48ef4aefe29f20be687a3a293676a
Shreyas Patil <luciferjones2015@gmail.com>
2024-04-28 03:45:16 +0530

    Made Some Changes in AiApp

--- AiApp/IntervueApp/views.py
+++ AiApp/IntervueApp/views.py
@@ -5,11 +5,62 @@ from django.conf import settings
 from django.http import JsonResponse
 import google.generativeai as genai
 from gtts import gTTS
-from pyresparser import ResumeParser as repr
+import nltk 
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+import speech_recognition as sr
+import pypdf
 # Create your views here.
 
 similarity_scores=[0.0]*3
+resume=""
 position=""
+answer=""
+follow_up_q=False
+model = genai.GenerativeModel('gemini-pro')
+genai.configure(api_key='AIzaSyB2ykTpIgDjPe59LxWAIw_6QYjLdwrmNAA')
+chat=model.start_chat(history=[])
+current_answer=""
+current_question=""
+fu_question=""
+
+def speak(text):
+    try:
+        os.remove(os.path.join(settings.MEDIA_ROOT, 'audio', 'question.mp3'))
+    except:
+        pass
+    tts = gTTS(text=text, lang='en', slow=False, tld='com', lang_check=False)
+    audio_path = os.path.join(settings.MEDIA_ROOT, 'audio', 'question.mp3')
+    tts.save(audio_path)
+    
+    
+    
+def get_answer():
+    
+    try:
+        os.remove(os.path.join(settings.MEDIA_ROOT, 'audio', 'answer.wav'))
+    except:
+        pass
+    
+    audio_file_path = os.path.join(settings.MEDIA_ROOT, 'audio', 'answer.wav')
+    r = sr.Recognizer()
+    said = ""
+    try:
+        with sr.AudioFile(audio_file_path) as source:
+            audio_data = r.record(source)
+            said = r.recognize_google(audio_data)
+    
+    except sr.UnknownValueError:
+        print("Speech Recognition could not understand audio")
+        return -1
+    
+    except sr.RequestError as e:
+        print("Could not request results from Speech Recognition service; {0}".format(e))
+        return -1
+
+    return said
+
+
 
 def index(request):
     if request.method == "GET":
@@ -30,7 +81,7 @@ def upload(request):
                 for chunk in uploaded_file.chunks():
                     destination.write(chunk)
         process_media_folder()   
-        return redirect()
+        return redirect(play_audio)
       
     return render(request, 'IntervueApp/upload.html')
 
@@ -40,11 +91,21 @@ def record_and_process_audio(request):
     elif request.method == 'POST':
         if request.FILES.get('audio'):
             audio_file = request.FILES['audio']
-            os.makedirs(os.path.dirname(settings.MEDIA_ROOT + '\\audio\\audio.wav'), exist_ok=True)
-            with open(settings.MEDIA_ROOT + '\\audio\\audio.wav', 'wb') as f:
+            os.makedirs(os.path.dirname(settings.MEDIA_ROOT + '\\audio\\answer.wav'), exist_ok=True)
+            with open(settings.MEDIA_ROOT + '\\audio\\answer.wav', 'wb') as f:
                 for chunk in audio_file.chunks():
                     f.write(chunk)
-            return redirect(play_audio)
+            current_answer = get_answer()
+            response = chat.send_message_async(f"Give an ideal answer, for the question {current_question} considering the skills {resume}")
+            ideal_nu_answer= response.text.replace('\n', '').replace('```', '') 
+            tokens1 = set(word_tokenize(current_answer.lower())) - set(stopwords.words('english'))
+            tokens2 = set(word_tokenize(ideal_nu_answer.lower())) - set(stopwords.words('english'))
+            similarity_score = len(tokens1.intersection(tokens2)) / len(tokens1.union(tokens2))
+            if similarity_score < 50:
+                follow_up_q = True
+            else:
+                follow_up_q = False
+            similarity_scores.append(similarity_score)
         else:
             return JsonResponse({'status': 'error', 'message': 'No audio file found'})
     else:
@@ -52,24 +113,30 @@ def record_and_process_audio(request):
     
     
 def play_audio(request):
-        return render(request, 'IntervueApp/play_audio.html')
+    if follow_up_q:
+        response = chat.send_message(f"You are a recruiter at a Company, ask one question for {position} in a company based on the skills make that question technical. the Skillset of the person is {resume}")
+        current_question = response.text.replace('\n', '').replace('```', '')
+        speak(current_question)
+    else:
+        response = chat.send_message(f"Ask a follow up question for {position} in a company. the Skillset of the person is{resume},reviewing the previous answer")
+        fu_question = response.text.replace('\n', '').replace('```', '')
+        speak(fu_question)
+    return render(request, 'IntervueApp/play_audio.html')
+
+
 
 def process_media_folder():
     media_folder = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'media')
-    
     if not os.path.exists(media_folder):
         print("Media folder does not exist.")
         return
-    
     files = os.listdir(media_folder)
-    
     pdf_files = [file for file in files if file.endswith('.pdf')]
     if pdf_files:
         print("PDF files found in the media folder:")
         for pdf_file in pdf_files:
             print("- " + pdf_file)
         process_pdf_files(pdf_files)
-    
     img_files = [file for file in files if file.endswith(('.jpg', '.jpeg', '.png', '.gif'))]
     if img_files:
         print("Image files found in the media folder:")
@@ -77,10 +144,23 @@ def process_media_folder():
             print("- " + img_file)
         process_image_files(img_files)
 
+
 def process_pdf_files(pdf_files):
-    data = repr(pdf_files[0]).get_extracted_data()
-    name = data['name']
-    skills= data['skills']
+    for pdf_file in pdf_files:
+        pdf_path = os.path.join(settings.MEDIA_ROOT, 'pdf', pdf_file)
+
+        text = ""
+        with open(pdf_path, 'rb') as file:
+            pdf_reader = pypdf.PdfReader(pdf_path)
+            for page_num in range(len(pdf_reader.pages)):
+                page = pdf_reader.pages[page_num]
+                text += page.extract_text()
+                
+    resume = text
+
+
+
 
 def process_image_files(img_files):
     print("Processing image files...")
+
